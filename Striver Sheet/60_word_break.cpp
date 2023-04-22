1373db6a98544ef00cefad1489d52d98f652cd40
DIVYANSH GARG <dishugarg1511@gmail.com>
2023-04-22 07:38:14 +0530

    update

--- Striver Sheet/60_word_break.cpp
+++ Striver Sheet/60_word_break.cpp
@@ -1,1 +1,24 @@
-//TODO
+#include<bits/stdc++.h>
+using namespace std;
+
+int WordBreak(string s , vector<string> v){
+    int n = s.length();
+    vector<bool> dp(n+1 , false);
+    dp[0] = true;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if ((dp[j]) && find(v.begin() , v.end() , s.substr(j , i-j)) != v.end())
+            {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp.back();
+}
+
+int main(){
+
+}

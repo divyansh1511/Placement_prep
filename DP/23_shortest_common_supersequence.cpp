d9e2196644685d1cbcf828c2f6f1eb247fa27b8e
DIVYANSH GARG <dishugarg1511@gmail.com>
2023-04-25 09:01:25 +0530

    new_day_8

---
+++ DP/23_shortest_common_supersequence.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int SCS(string s1 , string s2 , int n , int m){
+    int dp[n+1][m+1];
+    memset(dp , 0 , sizeof(dp));
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s1[i-1] == s2[j-1])
+            {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+    }
+    return m+n-dp[n][m];
+}
+
+int main(){
+
+}
